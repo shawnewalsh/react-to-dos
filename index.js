aca8985d240e2d25eee86fbce58a150dc9332975
Shawn Walsh <shawn.walsh@gmail.com>
2022-07-31 16:28:15 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }        
+  ])
+  const [value, setValue] = React.useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos,{text:value, isCompleted:false}];
+    setTodos(newTodos);
+    setValue('');
+  }
+  const removeTodo = e => {
+      const index = Number(e.target.id);
+      let temp = [...todos];
+      temp.splice(index,1);
+      setTodos(temp);
+    }
+    
+  return(
+    <>
+      {todos.map((todo, i) => <div key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
+    <form onSubmit={handleSubmit}>  
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo"
+        onChange={e => setValue(e.target.value)}
+        />
+    </form>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
